c688be7b3a4372eff71199b80cf4a87e5e442c15
NELSON BOCHE <75344016+nelsonbs85@users.noreply.github.com>
2024-07-18 15:18:43 -0600

    Update automatas.py

--- automatas.py
+++ automatas.py
@@ -1,37 +1,58 @@
-# Autómata
+import sys
 
-# Q = {q0,q1,q2,q3} conjunto de estados
-# F = {q3}Estados Finales
-# Lenguaje inicie en 0, termine en 1
-estado = 'q0'
-entrada =''
-estado_final ="q3"
-palabra = '0010101001'
-transciciones = [
-    ['q0','1','q2'],
-    ['q0','0','q1'],
-    ['q1','1','q3'],
-    ['q1','0','q1'],
-    ['q3','0','q1'],
-    ['q3','1','q3']
-]
-# for i in range(len(palabra)):
-#     print("#"+str(i)+"-"+palabra[i])
+# DEFINIENDO EL AUTÓMATA
+
+class Automata:
+    transiciones: ()
+    estados: []
+    finales: []
+    alfabeto:[]
+    inicial:''
+    estado_actual = ''; 
     
-def buscar_transicion(estado,entrada, transciciones):
-    newEstado = ""
-    for i in range(len(transciciones)):
-        if estado == transciciones[i][0] and entrada == transciciones[i][1]:
-            return transciciones[i][2]
-        # else:
-        #     return "No existe transcisión para esta Entrada/Estado"
+    def __init__(self,inicial, finales, transiciones,estados,alfabeto):
+        self.inicial = inicial;
+        self.finales = finales; 
+        self.transiciones = transiciones; 
+        self.estados = estados; 
+        self.alfabeto = alfabeto; 
 
-for i in range(len(palabra)):
-    entrada = palabra[i]
-    estado  =buscar_transicion(estado,entrada,transciciones)
+    def siguiente_estado(automata,estado,entrada):
+        for i in range(len(automata.transiciones)):
+            if estado == automata.transiciones[i][0] and entrada == automata.transiciones[i][1]:
+                #return transciciones[i][2]
+                return (automata.transiciones[i][2])
+    
+    def valida_palabra(automata, estado_actual,palabra):
+        longitud = len(palabra)
+        for i in range(longitud):
+            try:
+                band = automata.alfabeto.index(palabra[i])
+            except ValueError:
+                print("Entrada: ", palabra[i],  "no pertenece al Alfabeto")
+            if estado_actual == '':
+                estado_actual = inicial;
+            #realiza el movimiento de estados
+            estado_actual = automata.siguiente_estado(estado_actual,palabra[i])
+        return estado_actual
+        
+#llamada a la clase               
+transiciones= [['q1','a','q2'],
+                ['q2','b','q5'],
+                ['q3','a','q3'],
+                ['q4','b','q2'],
+                ['q5','b','q1']]
+estados= ['q0','q1','q2','q3','q4']
+finales= ['q0','q1']
+alfabeto=['a','b']
+inicial="q1"
 
-if (estado == estado_final):
-    print("Palabra Aceptada =)")
-else:
-    print("Palabra Rechazada =(")
-#print("Estado:" +estado +  " - Entrada:" +  entrada)
+#creación de instancia
+automata = Automata(inicial, finales, transiciones,estados,alfabeto)
+#print(automata.siguiente_estado('q2','a'))
+try:
+    finales.index(automata.valida_palabra(inicial,'abba'))
+    print("Palabra válida")
+except ValueError:
+    print ("Palabra no aceptada")
+
